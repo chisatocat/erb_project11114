b83fce2c2d7173a76067bbe96642c0eb71bff630
Dickens Au <dickens721@gmail.com>
2024-11-14 10:15:01 +0800

    20241114

---
+++ js/hero.js
@@ -0,0 +1,25 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add("active");
+    } else {
+      slide.classList.remove("active");
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Auto slide every 6 seconds
+setInterval(nextSlide, 6000);
---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("toggle-button").addEventListener("click", function () {
+  const navList = document.getElementById("nav-list");
+  navList.classList.toggle("active");
+
+  console.log("Hello World");
+});
